65784cf369729670f7043783329de9eb3900af99
Mariam Moataz <mariammansour556@gmail.com>
2023-02-01 22:21:57 +0200

    SecondSB

---
+++ testRepo/testRepo/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  testRepo
+//
+//  Created by Mariam Moataz on 01/02/2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func secondBtn(_ sender: Any) {
+        secondLabel.text = "Done"
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
